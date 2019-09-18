48043c02523b22048c0cd94da28d0d9d4117ee53
hushino <frostshock@outlook.com>
2019-09-18 07:49:26 -0300

    fix

--- src/components/persona/Viewpersona.js
+++ src/components/persona/Viewpersona.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Viewpersona(props) {
+    const [data, setData] = useState()
     const { dataIndex } = props.match.params
     const fetchData = () => axios.get(`http://localhost:8080/api/viewpersona/${dataIndex}`)
         .then(function (response) {
             console.log(response.data)
-
+            setData(response.data)
         })
         .catch(function (error) {
             console.log(error);
