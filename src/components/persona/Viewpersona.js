db67596ca651e8167d4d641be4fad8ea2c6fb671
hushino <frostshock@outlook.com>
2019-09-18 08:30:34 -0300

    id

--- src/components/persona/Viewpersona.js
+++ src/components/persona/Viewpersona.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Viewpersona(props) {
-    const [data, setData] = useState()
+    const [data, setData] = useState([])
     const { dataIndex } = props.match.params
     const fetchData = () => axios.get(`http://localhost:8080/api/viewpersona/${dataIndex}`)
         .then(function (response) {
@@ -18,11 +18,15 @@ function Viewpersona(props) {
 
         fetchData();
 
-    });
+    },[]);
 
     return (
         <div>
-            asdasd
+           { 
+            <li key={data.id}>
+              {data.nombre} : id  {data.id}
+            </li>
+            }
         </div>
     )
 
